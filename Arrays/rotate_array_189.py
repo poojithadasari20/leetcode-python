88aeb1819ca436c7d738641162881e80841342e3
poojithadasari20 <poojithadasari20@gmail.com>
2026-02-10 22:20:19 +0530

    Delete Arrays/rotate_array_189.py

--- Arrays/rotate_array_189.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        n=len(nums)
-        k=k%n
-
-        nums.reverse()
-        nums[:k]=reversed(nums[:k])
-        nums[k:]=reversed(nums[k:])
-        
