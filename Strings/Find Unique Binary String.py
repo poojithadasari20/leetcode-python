b9dbd9eaf3b719156819e8d7f60a31e7b3777b6c
poojithadasari20 <poojithadasari20@gmail.com>
2026-03-08 17:11:42 +0530

    Add solution for finding unique binary string
    Implemented the findDifferentBinaryString method to generate a unique binary string based on the input list.

---
+++ Strings/Find Unique Binary String.py
@@ -0,0 +1,18 @@
+LeetCode 1980 – Find Unique Binary String
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+
+
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        result=""
+        for i in range (len(nums)):
+            if nums[i][i]=='0':
+                result+='1'
+            else:
+                result+='0'
+        return result            
+
+        
